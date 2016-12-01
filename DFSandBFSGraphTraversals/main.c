076a1aa3cf21ea0b457f08281d1cd135f5c8e094
Drew Wolfe <d.wolfelogic@gmail.com>
2016-12-01 14:09:06 -0800

    Initial commit with error concerning addLinkAfter()

--- DFSandBFSGraphTraversals/main.c
+++ DFSandBFSGraphTraversals/main.c
@@ -1,15 +1,44 @@
-//
-//  main.c
-//  DFSandBFSGraphTraversals
-//
-//  Created by Drew Wolfe on 12/1/16.
-//  Copyright © 2016 Drew Wolfe. All rights reserved.
-//
-
+#include "graph.h"
+#include <string.h>
 #include <stdio.h>
+#include <assert.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#define NUM_GRAPHS 3
+
+int main()
+{
+    // Load graphs
+    Graph* graphs[NUM_GRAPHS];
+    char nameBuffer[256];
+    for (int i = 0; i < NUM_GRAPHS; ++i)
+    {
+        sprintf(nameBuffer, "graph%d.txt", i + 1);
+        graphs[i] = loadGraph(nameBuffer);
+    }
+    
+    // Run DFS and BFS on each graph
+    for (int graphNum = 0; graphNum < NUM_GRAPHS; ++graphNum)
+    {
+        Graph* g = graphs[graphNum];
+        printf("\nGraph %d (graph%d.txt)\n---\n", graphNum + 1, graphNum + 1);
+        printGraph(g);
+        printf("\n");
+        for (int i = 0; i < g->numVertices; ++i)
+        {
+            for (int j = i; j < g->numVertices; ++j)
+            {
+                Vertex* v1 = &g->vertexSet[i];
+                Vertex* v2 = &g->vertexSet[j];
+                printf("%d ... %d : ", v1->label, v2->label);
+                int dfs = dfsIterative(g, v1, v2);
+                int bfs = bfsIterative(g, v1, v2);
+                printf("DFS %s, BFS %s\n",
+                       (dfs == 1) ? "path" : "no path",
+                       (bfs == 1) ? "path" : "no path");
+            }
+        }
+        freeGraph(g);
+    }
+    
     return 0;
 }
